dffe03f6087aecb8fe1892f38028cdead13cb618
Jort Broekhof <jortdus@gmail.com>
2021-10-26 12:53:54 +0200

    Created a function to clean, filter and sort data from JSON array.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+var cleaning =[]
 const rawdata = fetch('./datasets/form-data.json')
     .then(results => results.json())
-    .then(data => data.forEach(function(item, index, array) {
-        console.log(item, index);
+    .then(data => data.forEach(function(v, i, data) {
+        cleaning.push(data[i]["Kaas is ook een zoogdier?"].toString().toLowerCase())
+        cleaning.sort().reverse()
     }));
 
-
+console.log(cleaning)
 
 
