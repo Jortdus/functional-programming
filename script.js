689fb9d803b5e239d9b1a71218b39035b87af7ef
Jort Broekhof <jortdus@gmail.com>
2021-10-29 13:22:58 +0200

    Got some basics of a D3 bar chart

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let zininInts = [];
+const zininScale = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
 const JSONfetch = fetch('./datasets/form-data.json')
 	.then(results => results.json())
 
@@ -9,7 +13,6 @@ function parseData() {
 			return data.map(obj => {
 				Object.keys(obj).forEach(key => {
 					obj[key] = normalizeAnswers(obj[key])
-					obj[key] = answersToNumbers(obj[key]);
 				})
 				return obj;
 			})
@@ -51,20 +54,76 @@ function normalizeAnswers(data) {
 }
 
 function answersToNumbers(data) {
-	return data.forEach(data => {
-		data['zinin']
-		console.log('oog')
+	zininInts = data.map(v => {
+		return parseInt(v.zinin);
 	});
-
 }
 
 parseData().then(cleanData => {
-	// console.log(cleanData[3]['zinin'])
-	// console.table(cleanData);
-	// d3.select('div')
-	// .selectAll('p')
-	// .data(cleanData)
-	// .enter()
-	// .append('p')
-	// .text();
+	answersToNumbers(cleanData)
+	// console.log(zininInts)
+	graphCreation()
+
 });
+
+function graphCreation() {
+
+	// set the dimensions and margins of the graph
+	var margin = {
+			top: 20,
+			right: 20,
+			bottom: 30,
+			left: 40
+		},
+		width = 960 - margin.left - margin.right,
+		height = 500 - margin.top - margin.bottom;
+
+	// set the ranges
+	var x = d3.scaleBand()
+		.range([0, width])
+		.padding(0.1);
+
+	var y = d3.scaleBand()
+		.range([0, height])
+		.padding(0.1);
+
+	var svg = d3.select("body").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform",
+			"translate(" + margin.left + "," + margin.top + ")");
+
+	console.log(zininInts)
+	console.log(d3.max(zininInts));
+
+	x.domain(zininScale);
+	y.domain(zininInts);
+
+	// append the rectangles for the bar chart
+	svg.selectAll(".bar")
+		.data(zininInts)
+		.enter().append("rect")
+		.attr("class", "bar")
+		.attr("x", function (d) {
+			return x(d);
+		})
+		.attr("width", x.bandwidth())
+		.attr("length", y.bandwidth())
+		.attr("y", function (d) {
+			return y(d);
+		})
+		.attr("height", function (d) {
+			return height - y(d);
+		});
+
+	// add the x Axis
+	svg.append("g")
+		.attr("transform", "translate(0," + height + ")")
+		.call(d3.axisBottom(x));
+
+	// add the y Axis
+	svg.append("g")
+		.call(d3.axisLeft(y));
+
+};
